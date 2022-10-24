1d5dffbef18a4f6b827e5d762ef1d48c97d0ca8c
Joey574 <joeysoroka@outlook.com>
2022-10-24 08:34:58 -0700

    10/24/22

--- src/GS05_02.java
+++ src/GS05_02.java
@@ -10,8 +10,16 @@ Pseudocode:
 Maintenance Log:
  */
 
-    public static void draw (int progress, String word, char [] guesses)
+    static char [] guesses = new char[0];
+    static char [] tempGuess = new char[0];
+    static int progress = 0;
+    static String word;
+
+
+    public static void draw ()
     {
+
+        boolean temp = false;
 
         if (progress > 0)
         {
@@ -69,30 +77,85 @@ Maintenance Log:
 
         for (int i = 0; i < word.length(); i++)
         {
+            temp = false;
             for (int q = 0; q < guesses.length; q++)
             {
                 if (word.charAt(i) == guesses[q])
                 {
                     System.out.print(word.charAt(i) + " ");
+                    temp = true;
                 }
-                else
-                {
-                    System.out.print("_ ");
-                }
+            }
+            if (!temp)
+            {
+                System.out.print("_ ");
             }
         }
 
     }
 
-    public static void charGuess(char [] guesses, String word)
+    public static void charGuess()
     {
+        boolean guessed = false;
+        boolean cGuess = false;
+
         Scanner r = new Scanner(System.in);
 
         String temp;
 
-        System.out.print("\nEnter char to guess: ");
-        temp = r.nextLine();
-        guesses = temp.charAt(0);
+        while(true)
+        {
+            System.out.print("\nEnter char to guess: ");
+            temp = r.nextLine();
+
+            for (int i = 0; i < guesses.length; i++)
+            {
+                if (temp.charAt(0) == guesses[i])
+                {
+                    guessed = true;
+                }
+            }
+
+            if (guessed)
+            {
+                System.out.print("\nAlready guessed that char");
+                continue;
+            }
+            else
+            {
+                tempGuess = new char [guesses.length];
+
+                for (int i = 0; i < guesses.length; i++)
+                {
+                    tempGuess[i] = guesses[i];
+                }
+
+                guesses = new char [guesses.length + 1];
+
+                for (int i = 0; i < tempGuess.length; i++)
+                {
+                    guesses[i] = tempGuess[i];
+                }
+
+                guesses[guesses.length - 1] = temp.charAt(0);
+            }
+
+            for (int i = 0; i < word.length(); i++)
+            {
+                if (guesses[guesses.length - 1] == word.charAt(i))
+                {
+                    cGuess = true;
+                    break;
+                }
+            }
+
+            if (!cGuess)
+            {
+                progress++;
+            }
+            break;
+        }
+
     }
 
 
@@ -100,12 +163,7 @@ Maintenance Log:
     {
         Scanner r = new Scanner(System.in);
 
-        String word;
-
-        int progress = 0;
         int temp;
-
-        char [] guesses = new char[1];
 
         boolean win = false;
         boolean lose = false;
@@ -114,23 +172,23 @@ Maintenance Log:
         System.out.print("Enter secret word: ");
         word = r.nextLine();
 
-        //while(!win && !lose)
-        //{
+       while(!win && !lose)
+       {
             if (progress > 5)
             {
                 lose = true;
             }
 
-            draw(progress, word, guesses);
+            draw();
 
             while(true)
             {
-                System.out.print("\n 1: Guess letter (cost 1 if failed)\n2: Guess word (cost 2 if failed)\nInput: ");
+                System.out.print("\n1: Guess letter (cost 1 if failed)\n2: Guess word (cost 2 if failed)\nInput: ");
                 temp = r.nextInt();
 
                 if (temp == 1)
                 {
-                    charGuess(guesses, word);
+                    charGuess();
                 }
                 else if (temp == 2)
                 {
@@ -144,7 +202,7 @@ Maintenance Log:
             }
 
 
-       // }
+       }
 
     }
 }
