99b5e8b39f55349e7b486b8ec41951c8edce9f8c
joey soroka <joeysoroka@outlook.com>
2022-11-07 23:23:20 -0800

    11/7/22

---
+++ src/Personal_Calculator.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Personal_Calculator
+{
+    public static int [][] circleLineIntercept(double r, int p1 [], int p2 [])
+    {
+
+    }
+
+    public static double distanceCalculator(int p1 [], int p2 [])
+    {
+        return Math.sqrt(((p1[0] - p2[0]) * (p1[0] - p2[0])) + ((p1[1] - p2[1]) * (p1[1] - p2[1])));
+    }
+
+    public static void main(String[] args)
+    {
+
+    }
+}
