3455052f876f6a90bb73f66ddb1f4795af88d483
joey soroka <joeysoroka@outlook.com>
2022-11-08 21:49:09 -0800

    11/8/22

--- src/GS07_05.java
+++ src/GS07_05.java
@@ -1,5 +1,10 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -13,6 +18,7 @@ public class GS07_05
 
         ArrayList <String> temp = new ArrayList<String>();
 
+        String x;
         String out = "";
         
         for (int i = 0; i < t.length(); i++)
@@ -20,43 +26,65 @@ public class GS07_05
             if (t.charAt(i) == ' ')
             {
                 stop = i;
-                temp.add(0, t.substring(start, stop)) ;
+
+                x = t.substring(start, stop);
+                x = x.strip();
+
+                temp.add(0, x);
                 start = i;
             }
         }
-        
+
+        if (t.charAt(t.length() - 1) != ' ')
+        {
+            temp.add(0, t.substring(start));
+        }
+
         for (int i = 0; i < temp.size(); i++)
         {
-            out += temp.get(i);
+            out += temp.get(i) + " ";
         }
-        
+
+        if (out.charAt(0) == ' ')
+        {
+            out = out.substring(1);
+        }
+
         return out;
     }
     
-    public static void main(String[] args) throws FileNotFoundException
-    {
+    public static void main(String[] args) throws IOException {
         String fileName = "GS07_05_File.txt";
 
         FileReader fr = new FileReader(fileName);
         Scanner lineScanner = new Scanner(fr);
 
-        ArrayList <String> File = new ArrayList<String>();
+        ArrayList <String> fileReversed = new ArrayList<String>();
 
         while (lineScanner.hasNextLine())
         {
-            File.add(0, lineScanner.nextLine());           
-        }
-        
-        for (int i = 0; i < File.size(); i++)
-        {
-            File.add(i, reverse(File.get(i)));
-            File.remove (i + 1);
+            fileReversed.add(0, lineScanner.nextLine());
         }
 
-        for (int i = 0; i < File.size(); i++)
+        for (int i = 0; i < fileReversed.size(); i++)
         {
-            System.out.println(File.get(i));
+            fileReversed.add(i, reverse(fileReversed.get(i)));
+            fileReversed.remove (i + 1);
         }
+
+        System.out.print(fileReversed);
+
+        fr.close();
+
+        FileChannel.open(Paths.get(fileName), StandardOpenOption.WRITE).truncate(0).close();
+
+        FileWriter fw = new FileWriter((fileName));
+
+        for (int i = 0; i < fileReversed.size(); i++)
+        {
+            fw.write(fileReversed.get(i) + "\n");
+        }
+
+        fw.close();
     }
 }
-
