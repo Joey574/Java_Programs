394dc7d7c7574f6d9a6f2a0f709637d6f0c1bd73
Joey574 <joeysoroka@outlook.com>
2023-01-06 08:34:57 -0800

    1/6/23

--- src/GS12_01.java
+++ src/GS12_01.java
@@ -3,12 +3,46 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class GS12_01
 {
+
+    static int binarySearchStringArray(ArrayList<String> in, String target) {
+        boolean complete = false;
+        int out = -1;
+        char [] firstLetters = new char [2];
+        firstLetters[0] = target.charAt(0);
+
+        int start = 0;
+        int end = in.size();
+
+        for (int i = 0; !complete; i++) {
+            int loc = (start + end) / 2;
+            if (Objects.equals(in.get(loc), target)) {
+                out = loc;
+                complete = true;
+            } else if (start == end || start > end) {
+                out = -loc;
+                complete = true;
+            } else {
+                firstLetters[1] = in.get(loc).charAt(0);
+                if (firstLetters[0] > firstLetters[1]) {
+                    start = loc;
+                } else {
+                    end = loc;
+                }
+            }
+        }
+        return out;
+    }
+
     public static void main(String[] args) throws IOException {
         String fileName = "GS12_01_File.txt";
+
+        int firstWordLoc;
+        int secondWordLoc;
 
         FileReader fr = new FileReader(fileName);
         Scanner lineScanner = new Scanner(fr);
@@ -20,6 +54,7 @@ public class GS12_01
             while (lineScanner.hasNext()) {
                 words.add(lineScanner.next());
             }
+            System.out.println(words);
         }
 
         fr.close();
@@ -34,6 +69,16 @@ public class GS12_01
         System.out.print("Enter second word: ");
         secondWord = r.nextLine();
 
-        Arrays.binarySearch()
+        firstWordLoc = binarySearchStringArray(words, firstWord);
+        secondWordLoc = binarySearchStringArray(words, secondWord);
+
+        System.out.println("First word location: " + firstWordLoc);
+        System.out.println("Second word location: " + secondWordLoc);
+
+        if (secondWordLoc - (firstWordLoc + 1) == 1) {
+            System.out.println("There is " + (secondWordLoc - (firstWordLoc + 1) + " word in between your inputted words"));
+        } else {
+            System.out.println("There are " + (secondWordLoc - (firstWordLoc + 1) + " words in between your inputted words"));
+        }
     }
 }
---
+++ src/GS12_02.java
@@ -0,0 +1,3 @@
+public class GS12_02
+{
+}
