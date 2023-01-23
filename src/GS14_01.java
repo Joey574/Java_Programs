64e855e800d3beb1f70cae8bb4cafbc5624ff0cf
Joey574 <joeysoroka@outlook.com>
2023-01-23 08:34:32 -0800

    1/23/23

--- src/GS14_01.java
+++ src/GS14_01.java
@@ -1,9 +1,8 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
-
+import java.lang.reflect.Array;
+import java.util.*;
 
 
 public class GS14_01
@@ -19,6 +18,24 @@ Maintenance Log:
 
 
 
+    public static boolean isEditDistance (String in1, String in2) {
+        for (int i = 0; i < in1.length() && i < in2.length(); i++) {
+            in1.charAt(i) == in2.charAt(i);
+        }
+    }
+
+    public static ArrayList<String> findNeighbors(String start, LinkedList<String> dictionary) {
+        ArrayList<String> neighbors = new ArrayList<String>();
+
+        for (String temp : dictionary) {
+            if (isEditDistance(temp, start)) {
+                neighbors.add(temp);
+            }
+        }
+
+        return neighbors;
+    }
+
 
     public static void main(String[] args) throws IOException {
         String fileName = "dictionarySorted.txt";
@@ -26,7 +43,7 @@ Maintenance Log:
         FileReader fr = new FileReader(fileName);
         Scanner lineScanner = new Scanner(fr);
 
-        ArrayList<String> words = new ArrayList<String>();
+        LinkedList<String> words = new LinkedList<>();
 
         while (lineScanner.hasNextLine())
         {
@@ -37,13 +54,22 @@ Maintenance Log:
 
         String firstWord;
         String secondWord;
+        int firstWordLoc;
+        int secondWordLoc;
+
+        HashMap<String, List<String>> EditNeighbors = new HashMap<String, List<String>>();
 
         Scanner r = new Scanner(System.in);
+
+
+        for (int i = 0; i < words.size(); i++) {
+            ArrayList<String> temp = findNeighbors(words.get(i), words);
+            EditNeighbors.put(words.get(i), temp);
+        }
 
         System.out.print("Enter starting word: ");
         firstWord = r.nextLine();
         System.out.print("Enter ending word: ");
         secondWord = r.nextLine();
-
     }
 }
